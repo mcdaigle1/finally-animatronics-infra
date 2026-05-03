a346a56c253205dc2c1a98d81759f5aa419a89e9
Michael Daigle <mcdaigle1@gmail.com>
2026-05-03 09:26:17 -0700

    fix git repo url in iam roles

--- terraform/envs/prod/apps/iam_roles.tf
+++ terraform/envs/prod/apps/iam_roles.tf
@@ -13,8 +13,11 @@ resource "aws_iam_role" "github_actions_role" {
       },
       Action = "sts:AssumeRoleWithWebIdentity",
       Condition = {
+        StringEquals = {
+          "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com"
+        },
         StringLike = {
-          "token.actions.githubusercontent.com:sub" = "repo:mcdaigle1/finally-animatronics:ref:refs/heads/main"
+          "token.actions.githubusercontent.com:sub" = "repo:mcdaigle1/finally-animatronics-api:*"
         }
       }
     }]
