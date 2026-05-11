b6877ebf89273535810f784c59cd76fa303d48f8
Michael Daigle <mcdaigle1@gmail.com>
2026-05-10 17:08:55 -0700

    add efs.tf

--- terraform/envs/prod/apps/efs.tf
+++ terraform/envs/prod/apps/efs.tf
@@ -7,14 +7,31 @@ resource "aws_efs_file_system" "shared_efs" {
 }
 
 resource "aws_efs_mount_target" "this" {
-  for_each = {
-    for subnet in aws_subnet.private :
-    subnet.id => subnet
-  }
+  for_each = toset(data.terraform_remote_state.base.outputs.private_subnet_ids)
 
   file_system_id  = aws_efs_file_system.shared_efs.id
-  subnet_id       = each.value.id
-  security_groups = [aws_security_group.efs.id]
+  subnet_id       = each.value
+  security_groups = [aws_security_group.efs_sg.id]
+}
+
+resource "aws_security_group" "efs_sg" {
+  name        = "finally-animatronics-efs"
+  description = "EFS access for EKS nodes"
+  vpc_id      = data.terraform_remote_state.base.outputs.vpc_id
+
+  tags = {
+    Name = "finally-animatronics-efs"
+  }
+}
+
+resource "aws_security_group_rule" "efs_ingress" {
+  type                     = "ingress"
+  from_port                = 2049
+  to_port                  = 2049
+  protocol                 = "tcp"
+
+  security_group_id        = aws_security_group.efs_sg.id
+  source_security_group_id = data.terraform_remote_state.base.outputs.node_security_group_id
 }
 
 resource "kubernetes_storage_class_v1" "efs_sc" {
@@ -26,7 +43,7 @@ resource "kubernetes_storage_class_v1" "efs_sc" {
 
   parameters = {
     provisioningMode = "efs-ap"
-    fileSystemId     = aws_efs_file_system.shared.id
+    fileSystemId     = aws_efs_file_system.shared_efs.id
     directoryPerms   = "700"
   }
 }
